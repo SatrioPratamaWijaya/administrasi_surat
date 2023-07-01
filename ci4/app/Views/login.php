4e1967f6a0225bd60ae1ce0dc4bb257aa8aa41bd
SatrioPratamaWijaya <satriopratamawijayaa@gmail.com>
2023-07-02 00:44:48 +0700

    membuat dan mengaplikasikan database untuk halaman login

--- ci4/app/Views/login.php
+++ ci4/app/Views/login.php
@@ -91,11 +91,11 @@
                 <form action="<?= base_url('auth/login') ?>" method="POST">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" required>
+                        <input type="email" placeholder="Masukan email" name="email" id="email" class="form-control" required>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" required>
+                        <input type="password" placeholder="Masukan password" name="password" id="password" class="form-control" required>
                     </div>
                     <div class="d-grid">
                         <button type="submit" class="btn btn-primary">Login</button>
